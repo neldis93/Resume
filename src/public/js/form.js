1c0bb35aeabcfb268709c87f63f2821427bc33b1
Neldis Barrios <neldis.dj56@gmail.com>
2020-09-11 16:02:49 +0200

    First commit

---
+++ src/public/js/form.js
@@ -0,0 +1,18 @@
+// function validar () {
+//     
+//     var form= document.form;
+//     if (form.firstname.value == 0){
+//         alert('El campo nombre esta vacio');
+//         form.firstname.value=""; //limpiar el campo name
+//         form.firstname.focus();
+//         return false;
+
+//     }
+//     alert('Mensaje enviado con exito')
+//     form.submit();
+
+// }
+
+
+
+
---
+++ src/routes/index.js
@@ -0,0 +1,48 @@
+const { Router } = require('express');
+const nodemailer = require('nodemailer');
+//const { Transform } = require('nodemailer/lib/xoauth2');
+const router = Router();
+
+
+router.post('/send-mail', async (req, res) => {
+    const { firstname, lastname, email, phone, message } = req.body;
+
+    contentHtml= `
+        <h1>User Information</h1>
+        <ul>
+            <li>First name: ${firstname}</li>
+            <li>Last name: ${lastname}</li>
+            <li>User email: ${email}</li>
+            <li>Phone: ${phone}</li>
+        </ul>
+        <p>${message}</p>
+    `;
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp-mail.outlook.com',
+        port: 587,
+        secure: false, // en false lo envia sin SSL
+        auth: {
+            user: process.env.USER,
+            pass: process.env.PASSWORD 
+        },
+        tls:{
+            rejectUnauthorized: false
+        }
+    });
+
+    const info= await transporter.sendMail({
+        from: "'Neldis server' <neldis12@hotmail.com>",
+        to: 'neldis.dj56@gmail.com',
+        subject: 'Website contact form',
+        html: contentHtml
+    });
+
+    
+    console.log('Message sent',info.messageId);
+    res.redirect('/');
+});
+
+
+
+module.exports = router;
