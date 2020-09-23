0429734e7cf2c0ac6ec24fe0d20265c7e3b7882e
Neldis Barrios <neldis.dj56@gmail.com>
2020-09-23 20:55:28 +0200

    form.js modifications

--- src/public/js/form.js
+++ src/public/js/form.js
@@ -1,18 +1,50 @@
-// function validar () {
-//     
-//     var form= document.form;
-//     if (form.firstname.value == 0){
-//         alert('El campo nombre esta vacio');
-//         form.firstname.value=""; //limpiar el campo name
-//         form.firstname.focus();
-//         return false;
+//import swal from 'sweetalert';
 
-//     }
-//     alert('Mensaje enviado con exito')
-//     form.submit();
+function val () {
+    var form = document.form;
 
-// }
+    if (form.firstname.value == 0){
+        swal({text:"The firtname field is required",button: false,timer:4000});
+        form.firstname.value="";
+        form.firstname.focus();
+        return false;
+    }
+    if (form.lastname.value == 0){
+      swal({text:"The lastname field is required",button: false,timer:4000}); 
+      form.lastname.value="";
+      form.lastname.focus();
+      return false;
+    
+    }
+    if (form.email.value == 0){
+      swal({text:"The email field is required",button: false,timer:4000}); 
+      form.email.value="";
+      form.email.focus();
+      return false;
+    }
+    if(form.phone.value == 0){
+      swal({text:"The phone field is required",button: false,timer:4000}); 
+      form.phone.value="";
+      form.phone.focus();
+      return false;
+    }
+    if (form.message.value == 0){
+      swal({text:"The message field is required",button: false,timer:4000});
+      form.message.value="";
+      form.message.focus();
+      return false;
 
+  }
+  swal({
+    title: 'Good!',
+    text: 'Message sent successfully',
+    icon: 'success',
+    button: false,
+    timer: 6000,
+
+  });
+  form.submit();
+}
 
 
 
--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,11 +1,11 @@
-    (function ($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
+            this.pathname.replace(/^\//, "") &&
             location.hostname == this.hostname
         ) {
             var target = $(this.hash);
